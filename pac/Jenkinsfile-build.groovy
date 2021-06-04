5c203ac66a580dff50a1a8b24b4d77df28fade1f
Daniel Velasco <dvelben@gmail.com>
2021-06-04 12:31:37 +0200

    First commit

--- pac/Jenkinsfile-build.groovy
+++ pac/Jenkinsfile-build.groovy
@@ -5,17 +5,31 @@ pipeline {
 
     parameters {
         string(name: 'VERSION', defaultValue: '1.0', description: '')
+        string(name: 'GIT_URL', defaultValue: '', description: '')
+        string(name: 'GIT_BRANCH', defaultValue: '', description: '')
     }
 
     environment {
+        GIT_URL="${params.GIT_URL}"
+        GIT_BRANCH="${params.GIT_BRANCH}"
         VERSION="${params.VERSION}"
         AWS_DEFAULT_REGION = 'eu-central-1'
     }
 
     stages {
+        stage('Get the code!') {
+            steps {
+                sh '''
+                rm -rf dummyapp
+                mkdir dummyapp
+                cd dummyapp
+                git clone --single-branch --branch $GIT_BRANCH $GIT_URL .
+                '''
+            }
+        }
         stage('Build') {
             steps {
-                dir ("${env.WORKSPACE}/src/") {
+                dir ("${env.WORKSPACE}/dummyapp/") {
                     sh '''
                     docker build -t evolved-5g/dummy-netapp .
                     '''
@@ -26,7 +40,7 @@ pipeline {
         stage('Publish') {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'docker_pull_cred', usernameVariable: 'ARTIFACTORY_USER', passwordVariable: 'ARTIFACTORY_CREDENTIALS')]) {
-                    dir ("${env.WORKSPACE}/src/") {
+                    dir ("${env.WORKSPACE}/dummyapp/") {
                         sh '''
                         docker login --username ${ARTIFACTORY_USER} --password "${ARTIFACTORY_CREDENTIALS}" dockerhub.hi.inet
                         docker image tag evolved-5g/dummy-netapp dockerhub.hi.inet/evolved-5g/dummy-netapp:${VERSION}.${BUILD_NUMBER}
