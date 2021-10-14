0a9c55baf40431f46cac72897bd0d7fcc141fe23
Alejandro Molina <alemosan@hotmail.com>
2021-10-14 13:33:49 +0200

    dummy validation pipeline add test stage

--- pac/Jenkins-validation.groovy
+++ pac/Jenkins-validation.groovy
@@ -14,24 +14,54 @@ pipeline {
     }
 
     stages {
-        stage('Get the container') {
+        stage('Get the container from the Artifactory') {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'docker_pull_cred', usernameVariable: 'ARTIFACTORY_USER', passwordVariable: 'ARTIFACTORY_CREDENTIALS')]) {
                     dir ("${env.WORKSPACE}") {
                         sh '''
                         docker login --username ${ARTIFACTORY_USER} --password "${ARTIFACTORY_CREDENTIALS}" dockerhub.hi.inet
-                        docker image pull --all-tags dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}
+                        docker image pull  dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}:latest
                         '''
                     }
                 }
             }
         }
+
+        stage('Execute the container in the platform (Docker)') {
+            steps {
+                dir ("${env.WORKSPACE}") {
+                        sh '''
+                        sudo docker run -d --name netapp -i evolved-5g/${NETAPP_NAME} 
+                        '''
+                    }
+                
+            }
+        }
+        stage('Execute the container in the platform (Docker)') {
+            steps {
+                dir ("${env.WORKSPACE}") {
+                        sh '''
+                        sudo docker run -d --name netapp -i evolved-5g/${NETAPP_NAME} 
+                        '''
+                    }
+                
+            }
+        }
+
         stage('Test stage') {
             steps {
                 dir ("${env.WORKSPACE}") {
                     sh '''
-                    ls
-                    pwd
+                    sudo docker exec -it ${NETAPP_NAME} curl localhost:8080
+                    '''
+                }
+            }
+        }
+        stage('Validation container') {
+            steps {
+                dir ("${env.WORKSPACE}") {
+                    sh '''
+                    sudo docker commit netapp evolved-5g/validation/${NETAPP_NAME} 
                     '''
                 }
             }
@@ -51,6 +81,17 @@ pipeline {
                 }
             }
         }
+        stage('Clean up docker enviroment') {
+            steps {
+                dir ("${env.WORKSPACE}") {
+                    sh '''
+                    sudo docker rm -vf $(sudo docker ps -a -q)
+                    sudo docker rmi -f $(sudo docker images -a -q)
+                    '''
+                }
+            }
+        }
+
     }
 }
 
