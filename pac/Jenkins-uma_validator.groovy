daa5f3b33265a15c768864b74035eff03cedab02
Alejandro Molina <alemosan@hotmail.com>
2021-11-16 12:41:48 +0100

    Adding an experiment with more network parameters

--- pac/Jenkins-uma_validator.groovy
+++ pac/Jenkins-uma_validator.groovy
@@ -65,7 +65,7 @@ pipeline {
                         sh '''
                         ExecutionId=`curl -X POST "https://10.11.23.220:8082/elcm/api/v0/run" -H "accept: application/json" -H "authorization: Basic ZXZvbHZlZDVncGFzczpldm9sdmVkNWc=" -H "Content-Type: application/json" --data "${MSG} " --insecure| jq -r .ExecutionId `
                         curl -X GET "https://10.11.23.220:8082/execution${ExecutionId}/json" -H "accept: application/json" -H "authorization: Basic ZXZvbHZlZDVncGFzczpldm9sdmVkNWc=" --insecure
-                        curl "http://10.11.23.220:5003/statistical_analysis/uma_mydb?experimentid=15012&measurement=server_5g&kpi=Jitter%20(ms)" > /tmp/output.json
+                        curl "http://10.11.23.220:5003/statistical_analysis/uma_mydb?experimentid=15012&measurement=server_5g&kpi=Jitter%20(ms)&kpi=Throughput%20(Mbps)&kpi=Packet%20Loss%20(%)" > /tmp/output.json
                         '''
                     }
             }
