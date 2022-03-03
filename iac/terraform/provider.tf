bc7567ed4f10ef1f785c7040b4b3f4131ace2f7a
Alejandro Molina <alemosan@hotmail.com>
2022-03-03 18:17:02 +0100

    prueba

--- iac/terraform/provider.tf
+++ iac/terraform/provider.tf
@@ -1,7 +1,6 @@
 provider "kubernetes" {
-  config_path    = "kubeconfig"
-  config_context = "evol5-capif/api-ocp-epg-hi-inet:6443/system:serviceaccount:evol5-capif:deployer"
-  insecure       = true
+  config_path = "kubeconfig"
+  insecure    = true
 }
 
 
