bbcf790ce196a940c80b32ff88e8b57fe5b4b8c2
Alejandro Molina <alemosan@hotmail.com>
2022-03-16 12:59:21 +0100

    bug 2

--- pac/Jenkins-deploy.groovy
+++ pac/Jenkins-deploy.groovy
@@ -234,18 +234,18 @@ pipeline {
     }    
 
 
-    // post {
-    //     cleanup{
-    //         /* clean up our workspace */
-    //         deleteDir()
-    //         /* clean up tmp directory */
-    //         dir("${env.workspace}@tmp") {
-    //             deleteDir()
-    //         }
-    //         /* clean up script directory */
-    //         dir("${env.workspace}@script") {
-    //             deleteDir()
-    //         }
-    //     }
-    // }
+    post {
+        cleanup{
+            /* clean up our workspace */
+            deleteDir()
+            /* clean up tmp directory */
+            dir("${env.workspace}@tmp") {
+                deleteDir()
+            }
+            /* clean up script directory */
+            dir("${env.workspace}@script") {
+                deleteDir()
+            }
+        }
+    }
 }
