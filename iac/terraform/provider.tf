6c78233d077e2102786c86413335408e0381df81
Alejandro Molina <alemosan@hotmail.com>
2022-03-29 18:31:25 +0200

    feat: multiprovider

--- iac/terraform/provider.tf
+++ iac/terraform/provider.tf
@@ -1,20 +1,19 @@
-provider "kubernetes" {
-  config_path    = "kubeconfig"
-  config_context = "evol5-capif/api-ocp-epg-hi-inet:6443/system:serviceaccount:evol5-capif:deployer"
-  insecure       = true
-}
+# provider "kubernetes" {
+#   config_path    = "kubeconfig"
+#   config_context = "evol5-capif/api-ocp-epg-hi-inet:6443/system:serviceaccount:evol5-capif:deployer"
+#   insecure       = true
+# }
 
 # provider "kubernetes" { for Athens
 #   config_path    = "~/kubeconfig"
 #   config_context = "kubernetes-admin@kubernetes"
 #   insecure       = true
 # }
-
-# provider "kubernetes" {
-#   config_path    = "CONFIG_PATH"
-#   config_context = "CONFIG_CONTEXT"
-#   insecure       = true
-# }
+provider "kubernetes" {
+  config_path    = "CONFIG_PATH"
+  config_context = "CONFIG_CONTEXT"
+  insecure       = true
+}
 
 
 
