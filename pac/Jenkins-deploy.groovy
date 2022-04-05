391c93b8dc77f946af939e9008a0463d009c9007
Alejandro Molina <alemosan@hotmail.com>
2022-04-05 16:30:53 +0200

    solved problem

--- pac/Jenkins-deploy.groovy
+++ pac/Jenkins-deploy.groovy
@@ -168,17 +168,15 @@ pipeline {
         stage ('Log into AWS ECR') {
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'evolved5g-pull', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-                        dir ("${env.WORKSPACE}/iac/terraform/") {
-                            sh '''
-                            kubectl delete secret docker-registry regcred --ignore-not-found
-                            kubectl create secret docker-registry regcred                                   \
-                            --docker-password=$(aws ecr get-login-password)                                 \
-                            --namespace=$NAMESPACE_NAME                                                     \
-                            --docker-server=709233559969.dkr.ecr.eu-central-1.amazonaws.com                 \
-                            --docker-username=AWS
-                            '''
-                        }
+                    dir ("${env.WORKSPACE}/iac/terraform/") {
+                        sh '''
+                        kubectl delete secret docker-registry regcred --ignore-not-found
+                        kubectl create secret docker-registry regcred                                   \
+                        --docker-password=$(                                     \
+                        --namespace=$NAMESPACE_NAME                                                     \
+                        --docker-server=709233559969.dkr.ecr.eu-central-1.amazonaws.com                 \
+                        --docker-username=AWS
+                        '''
                     }
                 }    
             }
