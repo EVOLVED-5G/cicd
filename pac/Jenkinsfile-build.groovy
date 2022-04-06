70bc534f586efb6b7084f2f74b47fc6a7893a419
Alejandro Molina <alemosan@hotmail.com>
2022-04-06 16:41:11 +0200

    style: error in aws ecr login

--- pac/Jenkinsfile-build.groovy
+++ pac/Jenkinsfile-build.groovy
@@ -47,7 +47,7 @@ pipeline {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'evolved5g-push', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                     dir ("${env.WORKSPACE}/iac/terraform/") {
                         sh '''
-                        $(aws ecr get-login-password)
+                        $(aws ecr get-login --no-include-email)
                         docker image tag evolved-5g/${NETAPP_NAME} 709233559969.dkr.ecr.eu-central-1.amazonaws.com/evolved5g:${NETAPP_NAME}-${VERSION}.${BUILD_NUMBER}
                         docker image tag evolved-5g/${NETAPP_NAME} 709233559969.dkr.ecr.eu-central-1.amazonaws.com/evolved5g:${NETAPP_NAME}-latest
                         docker image push --all-tags 
