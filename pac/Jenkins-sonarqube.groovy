6e849fcf21399d77015c47c3f484144af234d70e
Jesus Macias Portela <jesus.maciasportela@telefonica.com>
2022-05-23 12:50:42 +0200

    Add stages

--- pac/Jenkins-sonarqube.groovy
+++ pac/Jenkins-sonarqube.groovy
@@ -15,8 +15,7 @@ pipeline {
         string(name: 'GIT_NETAPP_URL', defaultValue: 'https://github.com/EVOLVED-5G/dummy-netapp', description: 'URL of the Github Repository')
         string(name: 'GIT_NETAPP_BRANCH', defaultValue: 'evolved5g', description: 'NETAPP branch name')
         string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
-        string(name: 'REPORTING', defaultValue: false, description: 'Save report into artifactory')
-
+        booleanParam(name: 'REPORTING', defaultValue: false, description: 'Save report into artifactory')
     }
 
     environment {
