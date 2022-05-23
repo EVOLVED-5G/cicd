20e18d381c671556b2506008d79f1f2e5fa0e0e7
Jesus Macias Portela <jesus.maciasportela@telefonica.com>
2022-05-23 11:00:45 +0200

    Add stages

--- pac/Jenkins-sonarqube.groovy
+++ pac/Jenkins-sonarqube.groovy
@@ -63,11 +63,13 @@ pipeline {
         stage("Quality Gate"){
             steps {
                 timeout(time: 10, unit: 'MINUTES') {
-                    def qg = waitForQualityGate()
-                    if (qg.status != 'OK') {
-                        error "Pipeline aborted due to quality gate failure: ${qg.status}"
-                        //unstable("There are Checkstyle issues")
-                    }
+                    sh '''
+                        def qg = waitForQualityGate()
+                        if (qg.status != 'OK') {
+                            error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                            //unstable("There are Checkstyle issues")
+                        }
+                    '''
                 }
             }
         }
