3961e6d7bf763c43336ec0e0d52eecc94b170a5a
Alejandro Molina <alemosan@hotmail.com>
2022-05-24 21:14:41 +0200

    Bug 4

--- pac/Jenkins-security-scan-docker-images.groovy
+++ pac/Jenkins-security-scan-docker-images.groovy
@@ -48,7 +48,7 @@ pipeline {
                                         -H 'Accept: application/json, text/plain, */*' \
                                         -k | jq '.children[].name' | grep '${NETAPP_NAME_LOWER}*'"
                     
-                    def image = sh(script: "curl -s $images" true, returnStdout: true).trim()
+                    def image = sh(script: "curl -s $images", returnStdout: true).trim()
                     image.tokenize().each { x ->
                         sh """  curl  -H "Content-Type: application/json"   -X POST "http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=${x}&update_wiki=true&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=md" """
                         sh """  curl  -H "Content-Type: application/json"   -X POST "http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=${x}&update_wiki=true&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=json" > report-tr-img-${NETAPP_NAME_LOWER}.json """
