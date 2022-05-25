38a5435e1144348a3a45325105325eb525b44f2b
Jesus Macias Portela <jesus.maciasportela@telefonica.com>
2022-05-25 10:55:23 +0200

    Fix validation

--- pac/Jenkins-security-scan-docker-images.groovy
+++ pac/Jenkins-security-scan-docker-images.groovy
@@ -28,9 +28,9 @@ pipeline {
             steps {
                 dir ("${env.WORKSPACE}/") {
                     sh '''
-                    git clone --single-branch --branch $GIT_NETAPP_BRANCH https://$TOKEN@github.com/Telefonica/Evolved5g-${NETAPP_NAME}                                                
-                    git clone --single-branch --branch $GIT_NETAPP_BRANCH $GIT_NETAPP_URL  
-                    rm -rf Evolved5g-${NETAPP_NAME}/* 
+                    git clone --single-branch --branch $GIT_NETAPP_BRANCH https://$TOKEN@github.com/Telefonica/Evolved5g-${NETAPP_NAME}
+                    git clone --single-branch --branch $GIT_NETAPP_BRANCH $GIT_NETAPP_URL
+                    rm -rf Evolved5g-${NETAPP_NAME}/*
                     cp -R ${NETAPP_NAME}/* Evolved5g-${NETAPP_NAME}/
                     cd Evolved5g-${NETAPP_NAME}/
                     git add .
@@ -42,12 +42,22 @@ pipeline {
         }
         stage('Launch Github Actions command') {
             steps {
-                script {    
-                    def images = sh(returnStdout: true, script: "curl http://artifactory.hi.inet/ui/api/v1/ui/nativeBrowser/docker/evolved-5g/ -u $PASSWORD_ARTIFACTORY | jq '.children[].name' | grep '${NETAPP_NAME_LOWER}*'").trim()
-                    images.tokenize().each { x ->
-                        sh(returnStdout: true, script: "curl -H 'Content-Type: application/json'  -X POST http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=dockerhub.hi.inet/evolved-5g/infolysisnetapp&update_wiki=true&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=md")
-                        sh(returnStdout: true, script: "curl -H 'Content-Type: application/json'  -X POST http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=dockerhub.hi.inet/evolved-5g/${x}&update_wiki=false&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=json" >> "report-tr-img-${NETAPP_NAME_LOWER}.json")
-                    }
+                dir ("${env.WORKSPACE}/") {
+                    sh '''
+                    images=$(curl http://artifactory.hi.inet/ui/api/v1/ui/nativeBrowser/docker/evolved-5g/ -u $PASSWORD_ARTIFACTORY | jq '.children[].name' | grep '${NETAPP_NAME_LOWER}*')
+
+                    for x in "${images[@]}"
+                    do
+                        curl -s -H 'Content-Type: application/json' -X POST http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=dockerhub.hi.inet/evolved-5g/$x&update_wiki=true&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=md
+                        curl -s -H 'Content-Type: application/json' -X POST http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=dockerhub.hi.inet/evolved-5g/$x&update_wiki=false&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=json > report-tr-img-${NETAPP_NAME_LOWER}.json
+                    done
+                    '''
+
+                    // def images = sh(returnStdout: true, script: "curl http://artifactory.hi.inet/ui/api/v1/ui/nativeBrowser/docker/evolved-5g/ -u $PASSWORD_ARTIFACTORY | jq '.children[].name' | grep '${NETAPP_NAME_LOWER}*'").trim()
+                    // images.tokenize().each { x ->
+                    //     sh(returnStdout: true, script: "curl -H 'Content-Type: application/json'  -X POST http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=dockerhub.hi.inet/evolved-5g/infolysisnetapp&update_wiki=true&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=md")
+                    //     sh(returnStdout: true, script: "curl -H 'Content-Type: application/json'  -X POST http://epg-trivy.hi.inet:5000/scan-image?token=fb1d3b71-2c1e-49cb-b04b-54534534ef0a&image=dockerhub.hi.inet/evolved-5g/${x}&update_wiki=false&repository=Telefonica/Evolved5g-${NETAPP_NAME}&branch=${GIT_NETAPP_BRANCH}&output_format=json" >> "report-tr-img-${NETAPP_NAME_LOWER}.json")
+                    // }
                 }
             }
         }
