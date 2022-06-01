009b2c39c40d23f7381a4859823e213344946486
Alejandro Molina <alemosan@hotmail.com>
2022-06-01 12:48:10 +0200

    remove post action

--- pac/Jenkinsfile-build.groovy
+++ pac/Jenkinsfile-build.groovy
@@ -172,19 +172,5 @@ pipeline {
             }
         }
     }
-    post {
-        cleanup{
-            /* clean up our workspace */
-            deleteDir()
-            /* clean up tmp directory */
-            dir("${env.workspace}@tmp") {
-                deleteDir()
-            }
-            /* clean up script directory */
-            dir("${env.workspace}@script") {
-                deleteDir()
-            }
-        }
-    }
 }
 
