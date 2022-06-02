ba6ec277fbb72d0dd00fe8c9bbd079205bc29d61
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-06-02 08:59:43 +0200

    Build pipeline (#7)
    feat: build pipeline plus new pipelines as building blocks
    Co-authored-by: Jesus Macias Portela <jesus.maciasportela@telefonica.com>

--- pac/Jenkins-sonarqube.groovy
+++ pac/Jenkins-sonarqube.groovy
@@ -1,31 +1,125 @@
-pipeline {
-    agent {
-        node {
-            label 'evol5-sonarqube'
-        }
-    }
-    options {
-        timeout(time: 30, unit: 'MINUTES')
-    }
-    environment {
-        //ARTIFACTORY_CRED=credentials('artifactory_credentials')
-        SCANNERHOME = tool 'Sonar Scanner 5';
-    }
-    stages {
+/*
+NTH: Create library with helper methods
+*/
+String netappName(String url) {
+    String url2 = url?:'';
+    String var = url2.substring(url2.lastIndexOf("/") + 1);
+    return var ;
+}
 
-        stage('SonarQube analysis') {
+pipeline {
+    agent { node {label 'evol5-slave2'}  }
+
+    parameters {
+        string(name: 'GIT_NETAPP_URL', defaultValue: 'https://github.com/EVOLVED-5G/dummy-netapp', description: 'URL of the Github Repository')
+        string(name: 'GIT_NETAPP_BRANCH', defaultValue: 'evolved5g', description: 'NETAPP branch name')
+        string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
+        booleanParam(name: 'REPORTING', defaultValue: false, description: 'Save report into artifactory')
+    }
+
+    environment {
+        // GIT_NETAPP_URL="${params.GIT_NETAPP_URL}"
+        // GIT_CICD_BRANCH="${params.GIT_CICD_BRANCH}"
+        // GIT_NETAPP_BRANCH="${params.GIT_NETAPP_BRANCH}"
+        SCANNERHOME = tool 'Sonar Scanner 5';
+        NETAPP_NAME = netappName("${params.GIT_NETAPP_URL}").toLowerCase()
+        SQ_TOKEN=credentials('SONARQUBE_TOKEN')
+        ARTIFACTORY_CRED=credentials('artifactory_credentials')
+        ARTIFACTORY_URL="http://artifactory.hi.inet/artifactory/misc-evolved5g/validation"
+    }
+
+    stages {
+        stage('Get the code!') {
             steps {
+                dir ("${WORKSPACE}/") {
                     sh '''
-                        sudo ${SCANNERHOME}/bin/sonar-scanner -X \
-                            -Dsonar.projectKey=Evolved5g-master\
-                            -Dsonar.projectBaseDir=/home/ams@hi.inet//sonar-scanning-examples/sonarqube-scanner/src/python/ \
-                            -Dsonar.host.url=http://10.95.133.49 \
-                            -Dsonar.login=40f1332530d31e2372160616f6a458b82c5e429d \
-                            -Dsonar.projectName=Evolved5g-master \
-                            -Dsonar.language=python \
-                            -Dsonar.sourceEncoding=UTF-8 \
+                    rm -rf $NETAPP_NAME
+                    mkdir $NETAPP_NAME
+                    cd $NETAPP_NAME
+                    git clone --single-branch --branch $GIT_NETAPP_BRANCH $GIT_NETAPP_URL .
+                    '''
+                }
+           }
+        }
+
+        //TODO: Create a project for each NETAPP
+        stage('SonarQube Analysis and Wait for Quality Gate') {
+            steps {
+                 dir ("${WORKSPACE}/") {
+                    withSonarQubeEnv('Evol5-SonarQube') {
+                        sh '''
+                            ${SCANNERHOME}/bin/sonar-scanner -X \
+                                -Dsonar.projectKey=Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH} \
+                                -Dsonar.projectBaseDir="${WORKSPACE}/${NETAPP_NAME}/" \
+                                -Dsonar.sources="${WORKSPACE}/${NETAPP_NAME}/src/" \
+                                -Dsonar.host.url=http://195.235.92.134:9000 \
+                                -Dsonar.login=$SQ_TOKEN \
+                                -Dsonar.projectName=Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH} \
+                                -Dsonar.language=python \
+                                -Dsonar.sourceEncoding=UTF-8
+                        '''
+                    }
+                }
+            }
+        }
+
+        stage('Get SonarQube Report') {
+            when {
+                expression {
+                    return REPORTING;
+                }
+            }
+            steps {
+                 dir ("${WORKSPACE}/") {
+                    //TODO: IMPROVE WAIT FOR REPORT READY
+                    sh '''
+                    sleep 15
+                    sonar-report \
+                        --sonarurl="http://195.235.92.134:9000" \
+                        --sonartoken="$SQ_TOKEN" \
+                        --qualityGateStatus="true" \
+                        --sonarcomponent="Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH}" \
+                        --project="Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH}" \
+                        --application="Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH}" \
+                        --sinceleakperiod="false" \
+                        --allbugs="true" > sonar-report_Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH}.html
                     '''
                 }
             }
         }
+
+        stage('Upload report to Artifactory') {
+            when {
+                expression {
+                    return REPORTING;
+                }
+            }
+            steps {
+                 dir ("${WORKSPACE}/") {
+                    sh '''
+                    report_file="sonar-report_Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH}.html"
+                    url="$ARTIFACTORY_URL/$NETAPP_NAME/$report_file"
+
+                    curl -v -f -i -X PUT -u $ARTIFACTORY_CRED \
+                        --data-binary @"$report_file" \
+                        "$url"
+
+                    cp $report_file $report_file.txt
+                    '''
+                }
+            }
+        }
+
     }
+    post {
+        always {
+            emailext attachmentsPattern: '**/sonar-report_Evolved5g-${NETAPP_NAME}-${GIT_NETAPP_BRANCH}.html.txt',
+                body: '''${SCRIPT, template="groovy-html.template"}''',
+                mimeType: 'text/html',
+                subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
+                from: 'jenkins-evolved5G@tid.es',
+                replyTo: "no-reply@tid.es",
+                recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+        }
+    }
+}
--- pac/Jenkinsfile-build.groovy
+++ pac/Jenkinsfile-build.groovy
@@ -1,58 +1,173 @@
 String netappName(String url) {
     String url2 = url?:'';
     String var = url2.substring(url2.lastIndexOf("/") + 1);
-    var= var.toLowerCase()
     return var ;
 }
+
 pipeline {
-    agent { node {label 'evol5-slave'}  }
+    agent { node {label 'evol5-openshift'}  }
 
     parameters {
         string(name: 'VERSION', defaultValue: '1.0', description: '')
-        string(name: 'GIT_URL', defaultValue: '', description: '')
-        string(name: 'GIT_BRANCH', defaultValue: '', description: '')
+        string(name: 'GIT_NETAPP_URL', defaultValue: 'https://github.com/EVOLVED-5G/dummy-netapp', description: 'URL of the Github Repository')
+        string(name: 'GIT_NETAPP_BRANCH', defaultValue: 'evolved5g', description: 'NETAPP branch name')
+        string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
     }
 
     environment {
-        GIT_URL="${params.GIT_URL}"
-        GIT_BRANCH="${params.GIT_BRANCH}"
+        GIT_NETAPP_URL="${params.GIT_NETAPP_URL}"
+        GIT_CICD_BRANCH="${params.GIT_CICD_BRANCH}"
+        GIT_NETAPP_BRANCH="${params.GIT_NETAPP_BRANCH}"
         VERSION="${params.VERSION}"
         AWS_DEFAULT_REGION = 'eu-central-1'
-        NETAPP_NAME = netappName("${params.GIT_URL}")
+        AWS_ACCOUNT_ID = credentials('AWS_ACCOUNT_NUMBER')
+        NETAPP_NAME = netappName("${params.GIT_NETAPP_URL}").toLowerCase()
+        DOCKER_VAR = false
     }
 
     stages {
         stage('Get the code!') {
             steps {
-                sh '''
-                rm -rf dummyapp
-                mkdir dummyapp
-                cd dummyapp
-                git clone --single-branch --branch $GIT_BRANCH $GIT_URL .
-                '''
+                dir ("${env.WORKSPACE}/") {
+                    sh '''
+                    rm -rf $NETAPP_NAME 
+                    mkdir $NETAPP_NAME 
+                    cd $NETAPP_NAME
+                    git clone --single-branch --branch $GIT_NETAPP_BRANCH $GIT_NETAPP_URL .
+                    '''
+                }
+           }
+        }
+
+        stage('Check if there is a docker-compose in the repository') {
+            steps {
+                script{
+                    DOCKER_VAR = fileExists "${env.WORKSPACE}/${NETAPP_NAME}/docker-compose.yml"
+                }
+                echo "env DOCKER VAR is ${DOCKER_VAR}"
+                
             }
         }
+        //NICE TO HAVE: Makefile to encapsulate docker and docker-compose commands
         stage('Build') {
+            when {
+                expression {
+                    return !"${DOCKER_VAR}".toBoolean() 
+                }
+            }                
             steps {
-                dir ("${env.WORKSPACE}/dummyapp/") {
+                dir ("${env.WORKSPACE}/${NETAPP_NAME}/") {
                     sh '''
-                    docker build -t evolved-5g/${NETAPP_NAME} .
+                    docker build -t ${NETAPP_NAME} .
                     '''
                 }
             }
         }
-
-        stage('Publish') {
+        stage('Build Docker Compose') {
+            when {
+                expression {
+                    return "${DOCKER_VAR}".toBoolean()
+                }
+            }  
+            steps {
+                dir ("${env.WORKSPACE}/${NETAPP_NAME}/") {
+                    sh '''
+                    docker-compose up --build --force-recreate -d
+                    '''
+                }
+            }
+        }
+        stage('Modify image name and upload to AWS') {
+            when {
+                expression {
+                    return "${DOCKER_VAR}".toBoolean() 
+                }
+            }     
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'evolved5g-push', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {               
+                    script {    
+                        def cmd = "docker ps --format '{{.Image}}'"
+                        def cmd2 = "docker ps --format '{{.Names}}'"
+                        def image = sh(returnStdout: true, script: cmd).trim()
+                        def name  = sh(returnStdout: true, script: cmd2).trim()
+                        sh '''$(aws ecr get-login --no-include-email)'''
+                        [image.tokenize(), name.tokenize()].transpose().each { x ->
+                            sh """ docker tag ${x[0]} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${x[1]}-${VERSION}"""
+                            sh """ docker tag ${x[0]} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${x[1]}-latest"""
+                            sh """ docker image push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${x[1]}-${VERSION}"""
+                            sh """ docker image push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${x[1]}-latest"""
+                        }
+                    }
+                }
+            }
+        }
+        stage('Publish in AWS - Dockerfile') {
+            when {
+                expression {
+                    return !"${DOCKER_VAR}".toBoolean() 
+                }
+            }    
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'evolved5g-push', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh '''
+                    $(aws ecr get-login --no-include-email)
+                    docker image tag ${NETAPP_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-${VERSION}
+                    docker image tag ${NETAPP_NAME} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-latest
+                    docker image push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-latest
+                    docker image push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-${VERSION}
+                    '''  
+                }   
+            }
+        }
+        stage('Modify container name to upload Docker-compose to Artifactory') {
+            when {
+                expression {
+                    return "${DOCKER_VAR}".toBoolean()  
+                }
+            }  
             steps {
                 withCredentials([usernamePassword(credentialsId: 'docker_pull_cred', usernameVariable: 'ARTIFACTORY_USER', passwordVariable: 'ARTIFACTORY_CREDENTIALS')]) {
-                    dir ("${env.WORKSPACE}/dummyapp/") {
-                        sh '''
-                        docker login --username ${ARTIFACTORY_USER} --password "${ARTIFACTORY_CREDENTIALS}" dockerhub.hi.inet
-                        docker image tag evolved-5g/${NETAPP_NAME} dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}:${VERSION}.${BUILD_NUMBER}
-                        docker image tag evolved-5g/${NETAPP_NAME} dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}:latest
-                        docker image push --all-tags dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}
-                        '''
+                    script {   
+                        sh ''' docker login --username ${ARTIFACTORY_USER} --password "${ARTIFACTORY_CREDENTIALS}" dockerhub.hi.inet '''
+                        def cmd = "docker ps --format '{{.Image}}'"
+                        def cmd2 = "docker ps --format '{{.Names}}'"
+                        def image = sh(returnStdout: true, script: cmd).trim()
+                        def name  = sh(returnStdout: true, script: cmd2).trim()
+                        sh '''$(aws ecr get-login --no-include-email)'''
+                        [image.tokenize(), name.tokenize()].transpose().each { x ->
+                            sh """ docker tag "${x[0]}" dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}-"${x[1]}":${VERSION}"""
+                            sh """ docker tag "${x[0]}" dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}-"${x[1]}":latest"""
+                            sh """ docker image push --all-tags dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}-"${x[1]}" """
+                        }
                     }
+                }               
+            }
+        }   
+        stage('Publish in Artefactory') {
+            when {
+                expression {
+                    return !"${DOCKER_VAR}".toBoolean() 
+                }
+            }   
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'docker_pull_cred', usernameVariable: 'ARTIFACTORY_USER', passwordVariable: 'ARTIFACTORY_CREDENTIALS')]) {
+                    sh '''
+                    docker login --username ${ARTIFACTORY_USER} --password "${ARTIFACTORY_CREDENTIALS}" dockerhub.hi.inet
+                    docker image tag ${NETAPP_NAME} dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}:${VERSION}
+                    docker image tag ${NETAPP_NAME} dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}:latest
+                    docker image push --all-tags dockerhub.hi.inet/evolved-5g/${NETAPP_NAME}
+                    '''
+                }
+            }
+        }
+        stage('Cleaning docker images and containers') {
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                    sh '''
+                    docker stop $(docker ps -q)
+                    docker system prune -a -f --volumes
+                    sudo rm -rf $WORKSPACE/$NETAPP_NAME/
+                    '''
                 }
             }
         }
