d6bc425da3ed0e1973524bcc64e4d7b06fa72e51
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-06-24 11:10:51 +0200

    feat: added new pipeline

---
+++ pac/Jenkins-final-report.groovy
@@ -0,0 +1,92 @@
+String netappName(String url) {
+    String url2 = url?:'';
+    String var = url2.substring(url2.lastIndexOf("/") + 1);
+    return var ;
+}
+
+pipeline {
+    agent { node {label 'evol5-openshift'}  }
+
+    parameters {
+        string(name: 'GIT_NETAPP_URL', defaultValue: 'https://github.com/EVOLVED-5G/dummy-netapp', description: 'URL of the Github Repository')
+        string(name: 'GIT_NETAPP_BRANCH', defaultValue: 'evolved5g', description: 'NETAPP branch name')
+        string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
+        booleanParam(name: 'REPORTING', defaultValue: false, description: 'Save report into artifactory')
+    }
+
+    environment {
+        GIT_NETAPP_URL="${params.GIT_NETAPP_URL}"
+        GIT_CICD_BRANCH="${params.GIT_CICD_BRANCH}"
+        GIT_NETAPP_BRANCH="${params.GIT_NETAPP_BRANCH}"
+        PASSWORD_ARTIFACTORY= credentials("artifactory_credentials")
+        NETAPP_NAME = netappName("${params.GIT_NETAPP_URL}")
+        NETAPP_NAME_LOWER = NETAPP_NAME.toLowerCase()
+        TOKEN = credentials('github_token_cred')
+        TOKEN_TRIVY = credentials('token_trivy')
+        TOKEN_EVOLVED = credentials('github_token_evolved5g')
+        ARTIFACTORY_CRED=credentials('artifactory_credentials')
+        ARTIFACTORY_URL="http://artifactory.hi.inet/artifactory/misc-evolved5g/validation"
+    }
+
+    stages {
+        stage('Get all jsons from artifactory'){
+            steps {
+                dir ("${env.WORKSPACE}/") {
+                    sh '''
+                    git clone https://$TOKEN@github.com/Telefonica/Evolved5g-${NETAPP_NAME}.wiki.git
+                    git clone $GIT_NETAPP_URL.wiki.git
+                    cp -R Evolved5g-${NETAPP_NAME}.wiki/* ${NETAPP_NAME}.wiki/
+                    cd ${NETAPP_NAME}.wiki/
+                    git add -A .
+                    git diff-index --quiet HEAD || git commit -m 'Addig Trivy report'
+                    git push  https://$TOKEN_EVOLVED@github.com/EVOLVED-5G/$NETAPP_NAME.wiki.git
+                    '''
+                }
+           }
+        }
+
+
+        stage('Upload report to Artifactory') {
+            when {
+                expression {
+                    return REPORTING;
+                }
+            }
+            steps {
+                 dir ("${WORKSPACE}/") {
+                    sh '''#!/bin/bash
+                    response=$(curl -v -s http://artifactory.hi.inet/ui/api/v1/ui/nativeBrowser/misc-evolved5g/validation/FogusNetApp/ -u $PASSWORD_ARTIFACTORY | jq ".children[].name" | grep  -i "json" | tr -d '"' )
+                    artifacts=($response)
+                    
+                    for x in "${artifacts[@]}"
+                    do  
+                        curl -sSf -u $PASSWORD_ARTIFACTORY -O 'http://artifactory.hi.inet:80/artifactory/misc-evolved5g/validation/FogusNetApp/$x'
+                    done
+                    '''
+                }
+            }
+        }
+    }
+    post {
+        always {
+            emailext body: '''${SCRIPT, template="groovy-html.template"}''',
+                mimeType: 'text/html',
+                subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
+                from: 'jenkins-evolved5G@tid.es',
+                replyTo: "no-reply@tid.es",
+                recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+        }
+        // cleanup{
+        //     /* clean up our workspace */
+        //     deleteDir()
+        //     /* clean up tmp directory */
+        //     dir("${env.workspace}@tmp") {
+        //         deleteDir()
+        //     }
+        //     /* clean up script directory */
+        //     dir("${env.workspace}@script") {
+        //         deleteDir()
+        //     }
+        // }
+    }
+}
