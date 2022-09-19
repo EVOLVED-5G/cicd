909028e1a5a96460271aac864e5d5fd4a11895e3
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-09-19 12:53:42 +0000

    build CAPIF

---
+++ pac/Jenkins-build-capif.groovy
@@ -0,0 +1,108 @@
+String netappName(String url) {
+    String url2 = url?:'';
+    String var = url2.substring(url2.lastIndexOf("/") + 1);
+    return var ;
+}
+
+pipeline {
+    agent { node {label 'evol5-openshift'}  }
+
+    parameters {
+        string(name: 'VERSION', defaultValue: '1.0', description: '')
+        string(name: 'GIT_NETAPP_URL', defaultValue: 'https://github.com/EVOLVED-5G/dummy-netapp', description: 'URL of the Github Repository')
+        string(name: 'GIT_NETAPP_BRANCH', defaultValue: 'evolved5g', description: 'NETAPP branch name')
+        string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
+    }
+
+    environment {
+        GIT_NETAPP_URL="${params.GIT_NETAPP_URL}"
+        GIT_CICD_BRANCH="${params.GIT_CICD_BRANCH}"
+        GIT_NETAPP_BRANCH="${params.GIT_NETAPP_BRANCH}"
+        VERSION="${params.VERSION}"
+        AWS_DEFAULT_REGION = 'eu-central-1'
+        AWS_ACCOUNT_ID = credentials('AWS_ACCOUNT_NUMBER')
+        NETAPP_NAME = netappName("${params.GIT_NETAPP_URL}").toLowerCase()
+        DOCKER_VAR = false
+    }
+    stages {
+        stage('Clean workspace') {
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                    sh '''
+                    docker stop $(docker ps -q)
+                    docker system prune -a -f --volumes
+                    sudo rm -rf $WORKSPACE/$NETAPP_NAME/
+                    '''
+                }
+            }
+        }
+        stage('Get the code!') {
+            steps {
+                dir ("${env.WORKSPACE}/") {
+                    sh '''
+                    rm -rf $NETAPP_NAME 
+                    mkdir $NETAPP_NAME 
+                    cd $NETAPP_NAME
+                    git clone --single-branch --branch $GIT_NETAPP_BRANCH $GIT_NETAPP_URL .
+                    '''
+                }
+           }
+        }
+        stage('Build CAPIF') { 
+            steps {
+                dir ("${env.WORKSPACE}/${NETAPP_NAME}/") {
+                    sh '''
+                    docker-compose up --build --force-recreate -d
+                    '''
+                }
+            }
+        }
+        stage('Modify image name and publish in AWS') {
+            when {
+                expression {
+                    return "${DOCKER_VAR}".toBoolean() 
+                }
+            }     
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'evolved5g-push', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {               
+                    script {    
+                        def cmd = "docker ps --format '{{.Image}}'"
+                        def cmd2 = "docker ps --format '{{.Names}}'"
+                        def image = sh(returnStdout: true, script: cmd).trim()
+                        def name  = sh(returnStdout: true, script: cmd2).trim()
+                        sh '''$(aws ecr get-login --no-include-email)'''
+                        [image.tokenize(), name.tokenize()].transpose().each { x ->
+                            sh """ docker tag ${x[0]} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-${x[1]}-${VERSION}"""
+                            sh """ docker tag ${x[0]} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-${x[1]}-latest"""
+                            sh """ docker image push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-${x[1]}-${VERSION}"""
+                            sh """ docker image push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/evolved5g:${NETAPP_NAME}-${x[1]}-latest"""
+                        }
+                    }
+                }
+            }
+        }  
+        stage('Publish in Artifacotry') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'docker_pull_cred', usernameVariable: 'ARTIFACTORY_USER', passwordVariable: 'ARTIFACTORY_CREDENTIALS')]) {
+                    dir ("${env.CAPIF_SERVICES_DIRECTORY}") {
+                        sh '''
+                            docker login --username ${ARTIFACTORY_USER} --password "${ARTIFACTORY_CREDENTIALS}" dockerhub.hi.inet
+                            docker-compose push
+                        '''
+                    }
+                }
+            }
+        }
+        stage('Cleaning docker images and containers') {
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                    sh '''
+                    docker stop $(docker ps -q)
+                    docker system prune -a -f --volumes
+                    sudo rm -rf $WORKSPACE/$NETAPP_NAME/
+                    '''
+                }
+            }
+        }
+    }
+}
