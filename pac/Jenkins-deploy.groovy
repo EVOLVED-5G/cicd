55d3194face4572d124c79ae7b281a932d228429
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-09-23 02:01:42 +0200

    integer

--- pac/Jenkins-deploy.groovy
+++ pac/Jenkins-deploy.groovy
@@ -38,7 +38,7 @@ pipeline {
 
     environment {
         GIT_BRANCH="${params.GIT_CICD_BRANCH}"
-        int APP_REPLICAS="${params.APP_REPLICAS}"
+        int APP_REPLICAS="${params.APP_REPLICAS}".toInteger()
         DUMMY_NETAPP_HOSTNAME="${params.DUMMY_NETAPP_HOSTNAME}"
         AWS_DEFAULT_REGION = 'eu-central-1'
         DEPLOYMENT_NAME = "fogus"
