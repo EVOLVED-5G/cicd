4837e09447a805fe053b1dc86aa53b494a346c04
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-11-22 12:25:54 +0100

    validation test

--- pac/Jenkins-Meta-NetApp-Validate.groovy
+++ pac/Jenkins-Meta-NetApp-Validate.groovy
@@ -157,7 +157,7 @@ pipeline {
             steps{
                 script {
                     def jobBuild = build job: '/001-CAPIF/Launch_Robot_Tests', wait: true, propagate: false,
-                                   parameters: [string(name: 'BRANCH_NAME', value: "develop"),
+                                   parameters: [string(name: 'BRANCH_NAME', value: "GHACKING-6064_adapt_robot_tests_new_flows"),
                                                 booleanParam(name: 'RUN_LOCAL_CAPIF', value: "False"),
                                                 string(name: 'CAPIF_HOSTNAME', value: "capif.apps.ocp-epg.hi.inet" )]
                     def jobResult = jobBuild.getResult()
