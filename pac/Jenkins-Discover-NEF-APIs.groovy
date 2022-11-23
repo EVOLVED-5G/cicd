be67fb59e3b01beb98fc291abf0004dd709465b3
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-11-23 18:33:32 +0100

    add post-conditions

--- pac/Jenkins-Discover-NEF-APIs.groovy
+++ pac/Jenkins-Discover-NEF-APIs.groovy
@@ -100,4 +100,18 @@ stages {
             }
         }
     }
+    post {
+        cleanup{
+            /* clean up our workspace */
+            deleteDir()
+            /* clean up tmp directory */
+            dir("${env.workspace}@tmp") {
+                deleteDir()
+            }
+            /* clean up script directory */
+            dir("${env.workspace}@script") {
+                deleteDir()
+            }
+        }
+    }
 }
