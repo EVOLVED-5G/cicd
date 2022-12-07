ef0b1a532d5a9495648dc3c6d4b6477417a64996
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-12-07 14:45:54 +0100

    added repositories

--- pac/Jenkins-security-scan-code.groovy
+++ pac/Jenkins-security-scan-code.groovy
@@ -97,7 +97,10 @@ pipeline {
                         commit=$(git rev-parse HEAD)
                         cd ..
 
-                        python3 utils/report_sonar_generator.py --template templates/scan-repo.md.j2 --json report-tr-repo-$NETAPP_NAME_LOWER.json --output report-tr-repo-$NETAPP_NAME_LOWER.md --repo ${GIT_NETAPP_URL} --branch ${GIT_NETAPP_BRANCH} --commit $commit
+                        urlT= https://github.com/EVOLVED-5G/$NETAPP_NAME/wiki/Telefonica-Evolved5g-$NETAPP_NAME
+                        versionT=0.35.0
+
+                        python3 utils/report_sonar_generator.py --template templates/scan-repo.md.j2 --json report-tr-repo-$NETAPP_NAME_LOWER.json --output report-tr-repo-$NETAPP_NAME_LOWER.md --repo ${GIT_NETAPP_URL} --branch ${GIT_NETAPP_BRANCH} --commit $commit --version $versionT --url $urlT
                         docker build  -t pdf_generator utils/docker_generate_pdf/.
                         docker run -v "$WORKSPACE":$DOCKER_PATH pdf_generator markdown-pdf -f A4 -b 1cm -s $DOCKER_PATH/utils/docker_generate_pdf/style.css -o $DOCKER_PATH/report-tr-repo-$NETAPP_NAME_LOWER.pdf $DOCKER_PATH/report-tr-repo-$NETAPP_NAME_LOWER.md
                         declare -a files=("json" "md" "pdf")
