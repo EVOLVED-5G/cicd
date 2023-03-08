4ac9ae043f7c65db4d60488bca3ebdcaf2630a22
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-03-08 12:01:12 +0100

    nef deploy and destroy

--- pac/Jenkins-NEF-deploy.groovy
+++ pac/Jenkins-NEF-deploy.groovy
@@ -35,6 +35,7 @@ pipeline {
     parameters {
         string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
         string(name: 'HOSTNAME', defaultValue: 'nef-5g.apps.ocp-epg.hi.inet', description: 'Hostname')
+        string(name: 'RELEASE_NAME', defaultValue: 'dummy-nef', description: 'Release name')
         choice(name: "DEPLOYMENT", choices: ["openshift", "kubernetes-athens", "kubernetes-uma"])  
     }
 
@@ -42,8 +43,7 @@ pipeline {
         GIT_BRANCH="${params.GIT_BRANCH}"
         HOSTNAME="${params.HOSTNAME}"
         AWS_DEFAULT_REGION = 'eu-central-1'
-        DEPLOYMENT_NAME = "nef"
-        NAMESPACE_NAME = "nef"
+        RELEASE_NAME = "${params.RELEASE_NAME}"
         DEPLOYMENT = "${params.DEPLOYMENT}"
 
     }
@@ -72,10 +72,11 @@ pipeline {
             steps {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'evolved5g-pull', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                     sh '''
-                    kubectl delete secret docker-registry regcred --ignore-not-found --namespace=$NAMESPACE_NAME
+                    kubectl delete secret docker-registry regcred --ignore-not-found --namespace=$RELEASE_NAME-${BUILD_NUMBER}
+                    kubectl create namespace $RELEASE_NAME-${BUILD_NUMBER}
                     kubectl create secret docker-registry regcred                                   \
                     --docker-password=$(aws ecr get-login-password)                                 \
-                    --namespace=$NAMESPACE_NAME                                                     \
+                    --namespace=$RELEASE_NAME-${BUILD_NUMBER}                                                    \
                     --docker-server=709233559969.dkr.ecr.eu-central-1.amazonaws.com                 \
                     --docker-username=AWS
                     '''
@@ -90,8 +91,23 @@ pipeline {
             }
             steps {
                 dir ("${env.WORKSPACE}") {
-                    sh '''
-                    helm upgrade --install --debug --kubeconfig /home/contint/.kube/config --create-namespace -n $NAMESPACE_NAME --wait $DEPLOYMENT_NAME ./cd/helm/$DEPLOYMENT_NAME/ --set nef_hostname=$HOSTNAME --atomic
+                    sh '''#!/bin/bash
+                           OUTPUT=($(helm ls --all-namespaces -q -f $RELEASE_NAME))
+                           echo "$OUTPUT"
+                           ARRAY=$(declare -p OUTPUT | grep -q '^declare -a' && echo array || echo no array)
+                            if [[ $ARRAY == "array" ]]; then
+                                if [[ " ${OUTPUT[@]} " =~ " ${RELEASE_NAME} " ]]; then
+                                    echo "Release name $RELEASE_NAME already exists, use another release name"
+                                    exit 1
+                                else
+                                    echo "applying helm"
+                                    helm upgrade --install --debug --kubeconfig /home/contint/.kube/config \
+                                    --create-namespace -n $RELEASE_NAME-${BUILD_NUMBER} \
+                                    --wait $RELEASE_NAME ./cd/helm/nef/ \
+                                    --set nef_hostname=$HOSTNAME --set env=$DEPLOYMENT \
+                                    --atomic
+                                fi
+                            fi
                     '''
                 }
             }
@@ -105,7 +121,11 @@ pipeline {
             steps {
                 dir ("${env.WORKSPACE}") {
                     sh '''
-                    helm upgrade --install --debug --create-namespace -n evol5-$NAMESPACE_NAME --wait $DEPLOYMENT_NAME ./cd/helm/$DEPLOYMENT_NAME/ --set nef_hostname=$HOSTNAME --atomic
+                    helm upgrade --install --debug \
+                    -n evol5-$RELEASE_NAME --wait \
+                    $RELEASE_NAME ./cd/helm/nef/ --set nef_hostname=$HOSTNAME \
+                    --set env=$DEPLOYMENT \
+                    --atomic
                     '''
                 }
             }
--- pac/Jenkins-NEF-destroy.groovy
+++ pac/Jenkins-NEF-destroy.groovy
@@ -34,6 +34,7 @@ pipeline {
 
     parameters {
         string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
+        string(name: 'RELEASE_NAME', defaultValue: 'dummy-nef', description: 'Release name')
         choice(name: "DEPLOYMENT", choices: ["openshift", "kubernetes-athens", "kubernetes-uma"])  
     }
 
@@ -41,8 +42,7 @@ pipeline {
         GIT_BRANCH="${params.GIT_BRANCH}"
         HOSTNAME="${params.HOSTNAME}"
         AWS_DEFAULT_REGION = 'eu-central-1'
-        DEPLOYMENT_NAME = "nef"
-        NAMESPACE_NAME = "nef"
+        RELEASE_NAME = "${params.RELEASE_NAME}"
         DEPLOYMENT = "${params.DEPLOYMENT}"
 
     }
@@ -71,7 +71,9 @@ pipeline {
             steps {
                 dir ("${env.WORKSPACE}") {
                     sh '''
-                    helm uninstall --debug --kubeconfig /home/contint/.kube/config $DEPLOYMENT_NAME -n $NAMESPACE_NAME --wait
+                    NAMESPACE=$(helm ls --all-namespaces -f $RELEASE_NAME | awk 'NR==2{print $2}')
+                    echo $NAMESPACE
+                    helm uninstall --debug --kubeconfig /home/contint/.kube/config $RELEASE_NAME -n $NAMESPACE --wait
                     '''
                 }
             }
@@ -85,7 +87,7 @@ pipeline {
             steps {
                 dir ("${env.WORKSPACE}") {
                     sh '''
-                    helm uninstall --debug $DEPLOYMENT_NAME -n evol5-$NAMESPACE_NAME --wait
+                    helm uninstall --debug $RELEASE_NAME -n evol5-$NAMESPACE_NAME --wait
                     '''
                 }
             }
