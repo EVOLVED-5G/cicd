a1677a0973907ba8537f749fd4ed9399fef511d1
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-03-10 11:45:44 +0100

    Update Jenkins-NEF-deploy.groovy

--- pac/Jenkins-NEF-deploy.groovy
+++ pac/Jenkins-NEF-deploy.groovy
@@ -35,7 +35,7 @@ pipeline {
     parameters {
         string(name: 'GIT_CICD_BRANCH', defaultValue: 'develop', description: 'Deployment git branch name')
         string(name: 'HOSTNAME', defaultValue: 'nef-5g.apps.ocp-epg.hi.inet', description: 'Hostname')
-        string(name: 'RELEASE_NAME', defaultValue: 'dummy-nef', description: 'Release name')
+        string(name: 'RELEASE_NAME', defaultValue: 'nef', description: 'Release name')
         choice(name: "DEPLOYMENT", choices: ["openshift", "kubernetes-athens", "kubernetes-uma"])  
     }
 
