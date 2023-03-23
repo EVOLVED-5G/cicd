125bd50ad098304f6a4ae28f54dd248f280717c7
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-03-23 11:14:56 +0100

    checkport improved

--- utils/checkports/checkportscicd.py
+++ utils/checkports/checkportscicd.py
@@ -50,12 +50,12 @@ if __name__ == '__main__':
 
     try:
         git.Repo.clone_from(git_repository, repo_dir, branch=netapp_branch)
-        repo_docker_file = glob.glob(repo_dir + 'docker-compose.y*ml')
+        repo_docker_compose_file = glob.glob(repo_dir + 'docker-compose.y*ml')
+        repo_docker_file = glob.glob(repo_dir + 'Dockerfile')
         
         # If there is no docker-compose yaml then, the python stops.
-        if (bool(repo_docker_file)):
-
-            repo_dir_docker = find_docker_compose(os.path.basename(repo_docker_file[0]), repo_dir)
+        if (bool(repo_docker_compose_file)):
+            repo_dir_docker = find_docker_compose(os.path.basename(repo_docker_compose_file[0]), repo_dir)
             with open(repo_dir_docker) as input:
                 for line in input:
                     if 'ports' in line:
@@ -63,6 +63,14 @@ if __name__ == '__main__':
 
             for port in NetApp_ports:
                 isOpen(netapp_host, port.split(":")[0])
+        elif (bool(repo_docker_file)):
+            repo_dir_docker = find_docker_compose(os.path.basename(repo_docker_file[0]), repo_dir)
+            with open(repo_dir_docker) as input:
+                for line in input:
+                    NetApp_ports += re.findall(r'EXPOSE \d+ ?\d*', line)
+
+            for port in NetApp_ports:
+                isOpen(netapp_host, port.split(" ")[1])
 
         else:
             print("\n\nNo docker-compose y(a)ml found in your repository.\nPlease make sure your NetApp (repository) has a docker-compose yaml file.")
