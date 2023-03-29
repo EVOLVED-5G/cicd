5e9490b198454f075f127b341cfa1030355cb3d2
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-03-29 15:54:34 +0200

    Update Jenkins-CAPIF-NEF-NETAPP-deploy.groovy

--- pac/Jenkins-CAPIF-NEF-NETAPP-deploy.groovy
+++ pac/Jenkins-CAPIF-NEF-NETAPP-deploy.groovy
@@ -118,38 +118,38 @@ pipeline {
                             mkdir ${BUILD_NUMBER}.d/
                             echo "setting up capif variables"
                             LATEST_VERSION=$(grep appVersion: ./cd/helm/capif/Chart.yaml)
-
+                            
                             sed -i -e "s/$LATEST_VERSION/appVersion: '$VERSION_CAPIF'/g" ./cd/helm/capif/Chart.yaml
-
+                            
                             yq -r -j ./cd/helm/helmfile.d/00-capif.yaml \
                             | jq '.releases[].name="capif-${BUILD_NUMBER}"' \
                             | jq '.releases[].namespace="capif-${BUILD_NUMBER}"' \
                             | jq '.releases[].values[1].nginx.nginx.env.capifHostname="$HOSTNAME_CAPIF"' \
                             | yq -P > ./${BUILD_NUMBER}.d/00-tmp-capif-${BUILD_NUMBER}.yaml
-
+                            
                             echo "./${BUILD_NUMBER}.d/00-tmp-capif-${BUILD_NUMBER}.yaml"
                             cat ./${BUILD_NUMBER}.d/00-tmp-capif-${BUILD_NUMBER}.yaml
-
+                            
                             echo "setting up nef variables"
                             yq -r -j ./cd/helm/helmfile.d/01-nef.yaml \
                             | jq '.releases[].name="nef-${BUILD_NUMBER}"' \
                             | jq '.releases[].namespace="nef-${BUILD_NUMBER}"' \
                             | jq '.releases[].values[1].backend.ingress.host="$HOSTNAME_NEF"' \
                             | yq -P > ./${BUILD_NUMBER}.d/01-tmp-nef-${BUILD_NUMBER}.yaml
-
+                            
                             echo "./${BUILD_NUMBER}.d/01-tmp-nef-${BUILD_NUMBER}.yaml"
                             cat ./${BUILD_NUMBER}.d/01-tmp-nef-${BUILD_NUMBER}.yaml
-
+                            
                             echo "setting up network-app variables"
                             yq -r -j ./cd/helm/helmfile.d/02-netapp.yaml \
                             | jq '.releases[].name="$FOLDER_NETWORK_APP-${BUILD_NUMBER}"' \
                             | jq '.releases[].namespace="network-app-${BUILD_NUMBER}"' \
                             | jq '.releases[].chart="../$FOLDER_NETWORK_APP/"' \
                             | jq '.releases[].values[1].env.fogusHostname="$HOSTNAME_NETAPP"' \
-                            |  jq '.releases[].values[1].env.environment="$DEPLOYMENT"' \
-                            | jq '.releases[].values[1].fe.replicas="$APP_REPLICAS"' \
+                            | jq '.releases[].values[1].env.environment="$DEPLOYMENT"' \
+                            | jq '.releases[].values[2].fe.replicas="$APP_REPLICAS"' \
                             | yq -P > ./${BUILD_NUMBER}.d/02-tmp-network-app-${BUILD_NUMBER}.yaml
-
+                            
                             echo "./${BUILD_NUMBER}.d/02-tmp-network-app-${BUILD_NUMBER}.yaml"
                             cat ./${BUILD_NUMBER}.d/02-tmp-network-app-${BUILD_NUMBER}.yaml
                             
