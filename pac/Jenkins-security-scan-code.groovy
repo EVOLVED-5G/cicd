a8c1fbbe481d96df751e0bdf68d1c1f0f173f6fe
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-03-30 11:02:25 +0200

    Fixing Scan Code Analysis

--- pac/Jenkins-security-scan-code.groovy
+++ pac/Jenkins-security-scan-code.groovy
@@ -88,7 +88,7 @@ pipeline {
                    cp -R Evolved5g-${NETAPP_NAME}.wiki/* ${NETAPP_NAME}.wiki/
                    cd ${NETAPP_NAME}.wiki/
                    git add -A .
-                   git diff-index --quiet HEAD || git commit -m 'Adding Trivy scan report'
+                   git diff-index --quiet HEAD || git commit -m \'Adding Trivy scan report\'
                    git push -u origin https://$TOKEN_EVOLVED@github.com/EVOLVED-5G/$NETAPP_NAME.wiki.git
                    '''
                }
