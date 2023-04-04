0e050966c30fcd664fb51dcf3c66a48149e484c5
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-04-04 14:25:45 +0200

    Add flag to scan secrets

--- pac/Jenkins-security-scan-secrets.groovy
+++ pac/Jenkins-security-scan-secrets.groovy
@@ -20,7 +20,7 @@ def getReportFilename(String netappNameLower) {
 }
 
 pipeline {
-    agent { node { label getAgent("${params.DEPLOYMENT }") == 'any' ? '' : getAgent("${params.DEPLOYMENT }")}}
+    agent { node { label getAgent("${params.DEPLOYMENT }") == 'any' ? '' : getAgent("${params.DEPLOYMENT }") } }
     options {
         timeout(time: 10, unit: 'MINUTES')
         retry(1)
@@ -171,8 +171,9 @@ pipeline {
     }
     post {
         always {
-            if (${params.SEND_DEV_MAIL} == true) {
-                emailext body: '''${SCRIPT, template="groovy-html.template"}''',
+            step('Send Mail To Developers') {
+                if (${ params.SEND_DEV_MAIL } == true) {
+                    emailext body: '''${SCRIPT, template="groovy-html.template"}''',
                 mimeType: 'text/html',
                 subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
                 from: 'jenkins-evolved5G@tid.es',
@@ -180,6 +181,7 @@ pipeline {
                 recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
             }
         }
+    }
         cleanup {
             /* clean up our workspace */
             deleteDir()
@@ -192,5 +194,5 @@ pipeline {
                 deleteDir()
             }
         }
-    }
+}
 }
