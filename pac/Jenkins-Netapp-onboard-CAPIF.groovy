babfa5bd05a403256686d2940fae045dddc03e3f
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-04-19 16:26:02 +0200

    Update Jenkins-Netapp-onboard-CAPIF.groovy

--- pac/Jenkins-Netapp-onboard-CAPIF.groovy
+++ pac/Jenkins-Netapp-onboard-CAPIF.groovy
@@ -125,7 +125,7 @@ pipeline {
                                 echo "INVOKER_LOG: $INVOKER_LOG"
                                 kubectl -n $TMP_NS_CAPIF get pods | grep nginx | awk '{print $1}' | xargs kubectl -n $TMP_NS_CAPIF logs 
                                 result=false
-                                exit 1
+                                #exit 1
                             fi
                             '''
                         } catch (Exception e) {
