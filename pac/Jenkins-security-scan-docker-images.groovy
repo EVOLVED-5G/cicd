adbd6f12fadad658547dc10f2574ede5751c4c79
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-04-21 11:57:09 +0200

    Fix report

--- pac/Jenkins-security-scan-docker-images.groovy
+++ pac/Jenkins-security-scan-docker-images.groovy
@@ -156,7 +156,7 @@ pipeline {
                         let count=$count+1
                         COUNTER="$count / $total_images"
                         urlT=https://github.com/EVOLVED-5G/$NETAPP_NAME/wiki/dockerhub.hi.inet-evolved-5g-$STAGE-$NETAPP_NAME_LOWER-$x
-                        python3 utils/report_generator.py --template templates/step-security-scan-docker-images.md.j2 --json ${REPORT_FILENAME}-$x.json --output ${REPORT_FILENAME}-$x.md --repo ${GIT_NETAPP_URL} --branch ${GIT_NETAPP_BRANCH} --commit commit --version $versionT --url $urlT --name $NETAPP_NAME --counter $COUNTER
+                        python3 utils/report_generator.py --template templates/step-security-scan-docker-images.md.j2 --json ${REPORT_FILENAME}-$x.json --output ${REPORT_FILENAME}-$x.md --repo ${GIT_NETAPP_URL} --branch ${GIT_NETAPP_BRANCH} --commit commit --version $versionT --url $urlT --name $NETAPP_NAME --counter "$COUNTER"
 
                         docker run -v "$WORKSPACE":$DOCKER_PATH ${PDF_GENERATOR_IMAGE_NAME}:${PDF_GENERATOR_VERSION} markdown-pdf -f A4 -b 1cm -s $DOCKER_PATH/utils/docker_generate_pdf/style.css -o $DOCKER_PATH/${REPORT_FILENAME}-$x.pdf $DOCKER_PATH/${REPORT_FILENAME}-$x.md || exit 1
 
