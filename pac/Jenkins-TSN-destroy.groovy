4b365e948518e7ec88fd7c62432813c3705effb7
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-05-23 10:49:07 +0200

    Create Jenkins-TSN-destroy.groovy

---
+++ pac/Jenkins-TSN-destroy.groovy
@@ -0,0 +1,105 @@
+String netappName(String url) {
+    String url2 = url?:'';
+    String var = url2.substring(url2.lastIndexOf("/") + 1);
+    var= var.toLowerCase()
+    return var ;
+}
+
+def getNamespace(deployment,name) {
+    String var = deployment
+    if("openshift".equals(var)) {
+        return "evol5-capif";
+    } else {
+        return name;
+    }
+}
+
+def getAgent(deployment) {
+    String var = deployment
+    if("openshift".equals(var)) {
+        return "evol5-openshift";
+    }else if("kubernetes-athens".equals(var)){
+        return "evol5-athens"
+    }else {
+        return "evol5-slave";
+    }
+}
+
+pipeline {
+    agent {node {label getAgent("${params.DEPLOYMENT}") == "any" ? "" : getAgent("${params.DEPLOYMENT}")}}
+    options {
+        timeout(time: 10, unit: 'MINUTES')
+        retry(1)
+    }
+
+    parameters {
+        string(name: 'GIT_CICD_BRANCH', defaultValue: 'main', description: 'Deployment git branch name')
+        string(name: 'RELEASE_NAME', defaultValue: 'tsn-frontend', description: 'Release name')
+        choice(name: "DEPLOYMENT", choices: ["openshift", "kubernetes-athens", "kubernetes-uma"])  
+    }
+
+    environment {
+        GIT_BRANCH="${params.GIT_BRANCH}"
+        HOSTNAME="${params.HOSTNAME}"
+        AWS_DEFAULT_REGION = 'eu-central-1'
+        RELEASE_NAME = "${params.RELEASE_NAME}"
+        DEPLOYMENT = "${params.DEPLOYMENT}"
+
+    }
+
+    stages {
+        stage ('Destroy/Uninstall app in kubernetes') {
+            when {
+                anyOf {
+                    expression { DEPLOYMENT == "kubernetes-athens" }
+                    expression { DEPLOYMENT == "kubernetes-uma" }
+                }
+            }
+            steps {
+                dir ("${env.WORKSPACE}") {
+                    sh '''
+                    NAMESPACE=$(helm ls --all-namespaces -f "^$RELEASE_NAME" | awk 'NR==2{print $2}')
+                    echo "NAMESPACE=$NAMESPACE"
+                    helm uninstall --debug --kubeconfig /home/contint/.kube/config $RELEASE_NAME -n $NAMESPACE --wait
+                    kubectl --kubeconfig /home/contint/.kube/config delete ns $NAMESPACE
+                    '''
+                }
+            }
+        }
+        stage ('Destroy/Uninstall app in Openshift') {
+            when {
+                allOf {
+                    expression { DEPLOYMENT == "openshift"}
+                }
+            }
+            environment {
+                TOKEN_NS_NEF = credentials("openshift-evol5-tsn-token")
+            }
+            steps {
+                dir ("${env.WORKSPACE}") {
+                    sh '''#!/bin/bash
+                    TMP_NS_TSN=evol5-tsn
+
+                    oc login --insecure-skip-tls-verify --token=$TOKEN_NS_TSN
+                    
+                    helm uninstall --debug $RELEASE_NAME -n $TMP_NS_TSN --wait
+                    '''
+                }
+            }
+        }
+    }                
+    post {
+        cleanup{
+            /* clean up our workspace */
+            deleteDir()
+            /* clean up tmp directory */
+            dir("${env.workspace}@tmp") {
+                deleteDir()
+            }
+            /* clean up script directory */
+            dir("${env.workspace}@script") {
+                deleteDir()
+            }
+        }
+    }
+}
