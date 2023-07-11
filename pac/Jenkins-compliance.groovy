5735ef71e834c2621a35873a93a850caebe12ff7
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-07-11 11:11:31 +0200

    fix compliance pipeline

--- pac/Jenkins-compliance.groovy
+++ pac/Jenkins-compliance.groovy
@@ -168,7 +168,7 @@ pipeline {
                         commit=$(git rev-parse HEAD)
                         cd ..
 
-                        python3 utils/report_licensecheck_generator.py --template templates/step-licensecheck.md.j2 --json ${WORKSPACE}/${NETAPP_NAME}/compliance_${NETAPP_NAME}_"$GIT_COMMIT".report.json --output ${REPORT_FILENAME}.md --repo ${GIT_NETAPP_URL} --branch ${GIT_NETAPP_BRANCH} --commit $commit
+                        python3 utils/report_licensecheck_generator.py --template templates/validation/step-licensecheck.md.j2 --json ${WORKSPACE}/${NETAPP_NAME}/compliance_${NETAPP_NAME}_"$GIT_COMMIT".report.json --output ${REPORT_FILENAME}.md --repo ${GIT_NETAPP_URL} --branch ${GIT_NETAPP_BRANCH} --commit $commit
                         docker run -v "$WORKSPACE":$DOCKER_PATH ${PDF_GENERATOR_IMAGE_NAME}:${PDF_GENERATOR_VERSION} markdown-pdf -f A4 -b 1cm -s $DOCKER_PATH/utils/docker_generate_pdf/style.css -o $DOCKER_PATH/${REPORT_FILENAME}.pdf $DOCKER_PATH/${REPORT_FILENAME}.md
                         declare -a files=("md" "pdf")
 
