149df32f8639011aca9e4925cacdce04b81808cb
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-07-18 15:03:44 +0200

    UMA validation pipeline recoded

--- pac/Jenkins-uma_validator.groovy
+++ pac/Jenkins-uma_validator.groovy
@@ -1,75 +1,123 @@
-String returnBody(){
-    return("""
-{
-  "Application": null,
-  "Automated": true,
-  "ExclusiveExecution": false,
-  "ExperimentType": "Standard",
-  "Extra": {},
-  "NSs": [],
-  "Parameters": {},
-  "Remote": null,
-  "RemoteDescriptor": null,
-  "ReservationTime": null,
-  "Scenario": null,
-  "Slice": null,
-  "TestCases": [
-    "Simple Test Case"
-  ],
-  "UEs": [],
-  "Version": "2.1.0"
+String netappName(String url) {
+    String url2 = url ?: ''
+    String var = url2.substring(url2.lastIndexOf('/') + 1)
+    return var
 }
-"""
-)
-    }
-pipeline {
-    agent { node {label 'evol5-slave2'}  }
 
-    // parameters {
-    //     // ADD more parameters
+String getPathAWS(deployment) {
+    String var = deployment
+    if ('verification'.equals(var)) {
+        return ''
+    }else if ('validation'.equals(var)) {
+        return 'validation'
+    }else {
+        return 'certification'
+    }
+}
+
+String getPath(deployment) {
+    String var = deployment
+    if ('verification'.equals(var)) {
+        return ''
+    }else if ('validation'.equals(var)) {
+        return 'validation/'
+    }else {
+        return 'certification/'
+    }
+}
+
+def getAgent(deployment) {
+    // String var = deployment
+    // if ('openshift'.equals(var)) {
+    //     return 'evol5-openshift'
+    // }else if ('kubernetes-athens'.equals(var)) {
+    //     return 'evol5-athens'
+    // }else {
+    //     return 'evol5-slave'
     // }
+    return 'evol5-slave'
+}
+
+def getReportFilename(String netappNameLower) {
+    return '000-report-platform-assesment-' + netappNameLower
+}
+
+String getArtifactoryUrl(phase) {
+    return 'http://artifactory.hi.inet/artifactory/misc-evolved5g/' + phase
+}
+
+
+String getHost(String url) {
+    URI uri = new URI(url);
+    String host = uri.getHost();
+    return host
+}
+
+pipeline {
+    agent { node { label getAgent("${params.DEPLOYMENT }") == 'any' ? '' : getAgent("${params.DEPLOYMENT }") } }
+    options {
+        retry(1)
+    }
+
+    parameters {
+        string(name: 'VERSION', defaultValue: '1.0', description: 'Version of NetworkApp')
+        string(name: 'GIT_NETAPP_URL', defaultValue: 'https://github.com/EVOLVED-5G/dummy-network-application', description: 'URL of the Github Repository')
+        string(name: 'GIT_NETAPP_BRANCH', defaultValue: 'evolved5g', description: 'NETAPP branch name')
+        string(name: 'GIT_CICD_BRANCH', defaultValue: 'main', description: 'Deployment git branch name')
+        string(name: 'BUILD_ID', defaultValue: '', description: 'value to identify each execution')
+        choice(name: 'STAGE', choices: ['verification', 'validation', 'certification'])
+        choice(name: 'DEPLOYMENT', choices: ['openshift', 'kubernetes-athens', 'kubernetes-uma'])
+        string(name: 'ELCM_URL', defaultValue: 'http://10.11.23.220:5551', description: 'URL to ELCM')
+        string(name: 'ANALYTICS_URL', defaultValue: 'http://10.11.23.220:5003', description: 'URL to Analytics')
+        booleanParam(name: 'REPORTING', defaultValue: false, description: 'Save report into artifactory')
+        booleanParam(name: 'SEND_DEV_MAIL', defaultValue: true, description: 'Send mail to Developers')
+    }
 
     environment {
-        MSG = returnBody()
+        GIT_NETAPP_URL = "${params.GIT_NETAPP_URL}"
+        GIT_CICD_BRANCH = "${params.GIT_CICD_BRANCH}"
+        GIT_NETAPP_BRANCH = "${params.GIT_NETAPP_BRANCH}"
+        VERSION = "${params.VERSION}"
+        NETAPP_NAME = netappName("${params.GIT_NETAPP_URL}")
+        NETAPP_NAME_LOWER = NETAPP_NAME.toLowerCase()
+        STAGE = "${params.STAGE}"
+        PATH_DOCKER = getPath("${params.STAGE}")
+        PATH_AWS = getPathAWS("${params.STAGE}")
+        CHECKPORTS_PATH = 'utils/checkports'
+        ARTIFACTORY_CRED = credentials('artifactory_credentials')
+        DOCKER_PATH = '/usr/src/app'
+        ARTIFACTORY_IMAGE_URL = getArtifactoryUrl("${env.PATH_DOCKER}")
+        ARTIFACTORY_URL = 'http://artifactory.hi.inet/artifactory/misc-evolved5g/validation'
+        REPORT_FILENAME = getReportFilename(NETAPP_NAME_LOWER)
+        PDF_GENERATOR_IMAGE_NAME = 'dockerhub.hi.inet/evolved-5g/evolved-pdf-generator'
+        PDF_GENERATOR_VERSION = 'latest'
+        ELCM_URL = "${params.ELCM_URL}"
+        ANALYTICS_URL = "${params.ANALYTICS_URL}"
+        ELCM_HOST = getHost(ELCM_URL)
+        ANALYTICS_HOST = getHost(ANALYTICS_URL)
     }
 
     stages {
-        
-        // For the moment, the token authorization is HARDCODED
-        // stage('Execute the experiment') {
-        //     steps {
-
-        //     user: evolved5gpass
-        //     pass: evolved5g
-
-        //     command: ACCESS_TOKEN=`curl -X GET "https://10.11.23.220:8082/auth/get_token" -H "accept: application/json" -H "authorization: Basic ZXZvbHZlZDVncGFzczpldm9sdmVkNWc=" --insecure| jq -r .result `
-
-        //     }   
-        // }
         stage('Check the connectivity with UMA ECLM') {
             steps {
                 dir ("${env.WORKSPACE}") {
-
-                sh '''
-                ping -c 3 10.11.23.220
-                '''
-
-                }   
+                    sh '''
+                    ping -c 3 ${ELCM_HOST}
+                    ping -c 3 ${ANALYTICS_HOST}
+                    '''
+                }
             }
         }
 
         stage('Execute the Experiment in the platform') {
             steps {
-                dir ("${env.WORKSPACE}") {
-
-                        sh '''
-                        ExecutionId=`curl -X POST "https://10.11.23.220:8082/elcm/api/v0/run" -H "accept: application/json" -H "authorization: Basic ZXZvbHZlZDVncGFzczpldm9sdmVkNWc=" -H "Content-Type: application/json" --data "${MSG} " --insecure| jq -r .ExecutionId `
-                        curl -X GET "https://10.11.23.220:8082/execution${ExecutionId}/json" -H "accept: application/json" -H "authorization: Basic ZXZvbHZlZDVncGFzczpldm9sdmVkNWc=" --insecure
-                        curl "http://10.11.23.220:5003/statistical_analysis/uma_mydb?experimentid=15012&measurement=server_5g&kpi=Jitter%20(ms)&kpi=Throughput%20(Mbps)&kpi=Packet%20Loss%20(%)" > /tmp/output.json
-                        '''
-                    }
+                dir ("${env.WORKSPACE}/utils/platform_assesment/") {
+                    sh '''
+                    pip3 install -r requirements.txt
+                    python3 platform_assesment.py ${ELCM_HOST} ${ANALYTICS_HOST} ${REPORT_FILENAME}
+                    '''
+                }
             }
         }
-
     }
 }
--- utils/platform_assesment/platform_assesment.py
+++ utils/platform_assesment/platform_assesment.py
@@ -1,9 +1,10 @@
 import requests
 import json
 from time import sleep
+import sys
 
-def get_kpi_value(execution_id, kpi):
-    url='http://10.11.23.220:5003/statistical_analysis/uma_mydb?experimentid=' + execution_id +'&measurement=' + kpi[0] + '&kpi=' + kpi[1]
+def get_kpi_value(analitics_url ,execution_id, kpi):
+    url= analitics_url + '/statistical_analysis/uma_mydb?experimentid=' + execution_id +'&measurement=' + kpi[0] + '&kpi=' + kpi[1]
     print(url)
     response = requests.get(url)
     if not response.ok:
@@ -32,62 +33,74 @@ descriptor = {
   "Version": "2.1.0"
 }
 
-host = 'http://10.11.23.220:5551'
 
-execution_run_url = host + '/experiment/run'
-
-response = requests.post(execution_run_url, json = descriptor)
-
-execution_id=''
-if not response.ok:
-    print(response)
-    raise('Execution of experiment fails')
-else:
-    execution_id_int = response.json().get('ExecutionId',None)
-    if(execution_id_int != None):
-        execution_id = str(execution_id_int)
-    print('ExecutionId: ' + execution_id)
-
-if execution_id == '' or execution_id == None:
-    raise('ExecutionId is not present on response, something fails')
-
-
-# execution_id = '550033'
-execution_status_url = host + '/execution/' + execution_id + '/status'
-
-execution_status=''
-status='Running'
-while status != 'Not Running':
-    print('Pollìng execution status')
-    response = requests.get(execution_status_url)
-    if not response.ok:
-        raise('Response error requesting status of ExecutionId ' + execution_id)
-    execution_status = response.json()
-    print(execution_status)
-    status = execution_status.get('Status')
-    if status != 'Not Running':
-        sleep(10)
-print('Polling finished')
-
-execution_kpis_url = host + '/execution/' + execution_id + '/kpis'
-response = requests.get(execution_kpis_url)
-if not response.ok:
-    raise('Response error requesting kpi list for ExecutionId ' + execution_id)
-
-execution_kpis = response.json()
-
-results=dict()
-for kpi in execution_kpis.get('KPIs'):
-    result=get_kpi_value(execution_id,kpi)
-    if result.get('experimentid') != {} and result.get('experimentid').get(execution_id) != {}:
-        results.update(result.get('experimentid').get(execution_id))
-    else:
-        results.update({ kpi[1]: { "status":"FAILED" } })
-        print('Measurement ' + kpi[0] + ' of kpi ' + kpi[1] + ' FAILS')
+if __name__ == '__main__':
     
-print(json.dumps(results, indent=4))
+    # total arguments
+    n = len(sys.argv)
+    
+    if n != 4:
+            print("expected: " + sys.argv[0] + " <elcm_url> <analitics_url> <output_filename>")
+            exit(255)
 
-with open('platform_assesment.json', 'w') as outfile:
-    json.dump(results, outfile)
+    elcm_url = sys.argv[1]
+    analitics_url = sys.argv[2]
+    output_filename = sys.argv[3]
+
+    execution_run_url = elcm_url + '/experiment/run'
+
+    response = requests.post(execution_run_url, json = descriptor)
+
+    execution_id=''
+    if not response.ok:
+        print(response)
+        raise('Execution of experiment fails')
+    else:
+        execution_id_int = response.json().get('ExecutionId',None)
+        if(execution_id_int != None):
+            execution_id = str(execution_id_int)
+        print('ExecutionId: ' + execution_id)
+
+    if execution_id == '' or execution_id == None:
+        raise('ExecutionId is not present on response, something fails')
+
+
+    # execution_id = '550033'
+    execution_status_url = elcm_url + '/execution/' + execution_id + '/status'
+
+    execution_status=''
+    status='Running'
+    while status != 'Not Running':
+        print('Pollìng execution status')
+        response = requests.get(execution_status_url)
+        if not response.ok:
+            raise('Response error requesting status of ExecutionId ' + execution_id)
+        execution_status = response.json()
+        print(execution_status)
+        status = execution_status.get('Status')
+        if status != 'Not Running':
+            sleep(10)
+    print('Polling finished')
+
+    execution_kpis_url = elcm_url + '/execution/' + execution_id + '/kpis'
+    response = requests.get(execution_kpis_url)
+    if not response.ok:
+        raise('Response error requesting kpi list for ExecutionId ' + execution_id)
+
+    execution_kpis = response.json()
+
+    results=dict()
+    for kpi in execution_kpis.get('KPIs'):
+        result=get_kpi_value(analitics_url, execution_id, kpi)
+        if result.get('experimentid') != {} and result.get('experimentid').get(execution_id) != {}:
+            results.update(result.get('experimentid').get(execution_id))
+        else:
+            results.update({ kpi[1]: { "status":"FAILED" } })
+            print('Measurement ' + kpi[0] + ' of kpi ' + kpi[1] + ' FAILS')
+        
+    print(json.dumps(results, indent=4))
+
+    with open(output_filename, 'w') as outfile:
+        json.dump(results, outfile)
 
 
