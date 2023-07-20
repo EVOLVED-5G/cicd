ef8d78f206aaefb2c0bb3b448461a96f4b946ca5
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-07-20 13:25:26 +0200

    new upgrades on uma validation pipeline

--- pac/Jenkins-uma_validator.groovy
+++ pac/Jenkins-uma_validator.groovy
@@ -15,17 +15,6 @@ String getPathAWS(deployment) {
     }
 }
 
-String getPath(deployment) {
-    String var = deployment
-    if ('verification'.equals(var)) {
-        return ''
-    }else if ('validation'.equals(var)) {
-        return 'validation/'
-    }else {
-        return 'certification/'
-    }
-}
-
 def getAgent(deployment) {
     // String var = deployment
     // if ('openshift'.equals(var)) {
@@ -83,12 +72,10 @@ pipeline {
         NETAPP_NAME = netappName("${params.GIT_NETAPP_URL}")
         NETAPP_NAME_LOWER = NETAPP_NAME.toLowerCase()
         STAGE = "${params.STAGE}"
-        PATH_DOCKER = getPath("${params.STAGE}")
         PATH_AWS = getPathAWS("${params.STAGE}")
         CHECKPORTS_PATH = 'utils/checkports'
         ARTIFACTORY_CRED = credentials('artifactory_credentials')
         DOCKER_PATH = '/usr/src/app'
-        ARTIFACTORY_IMAGE_URL = getArtifactoryUrl("${env.PATH_DOCKER}")
         ARTIFACTORY_URL = 'http://artifactory.hi.inet/artifactory/misc-evolved5g/validation'
         REPORT_FILENAME = getReportFilename(NETAPP_NAME_LOWER)
         PDF_GENERATOR_IMAGE_NAME = 'dockerhub.hi.inet/evolved-5g/evolved-pdf-generator'
@@ -100,6 +87,42 @@ pipeline {
     }
 
     stages {
+        stage('Certification Stage') {
+            steps {
+                script {
+                    if( "${STAGE}" != 'certification' ) {
+                        currentBuild.result = 'ABORTED'
+                        error("This job will be only executed on Certification Stage.")
+                        return
+                    }
+                }
+            }
+        }
+        stage('Prepare pdf generator tools') {
+            when {
+                expression {
+                    return REPORTING
+                }
+            }
+            options {
+                retry(2)
+            }
+
+            steps {
+                dir("${env.WORKSPACE}") {
+                    withCredentials([usernamePassword(
+                    credentialsId: 'docker_pull_cred',
+                    usernameVariable: 'USER',
+                    passwordVariable: 'PASS'
+                )]) {
+                        sh '''
+                        docker login --username ${USER} --password ${PASS} dockerhub.hi.inet
+                        docker pull ${PDF_GENERATOR_IMAGE_NAME}:${PDF_GENERATOR_VERSION}
+                        '''
+                }
+                }
+            }
+        }
         stage('Check the connectivity with UMA ECLM') {
             steps {
                 dir ("${env.WORKSPACE}") {
@@ -116,9 +139,75 @@ pipeline {
                 dir ("${env.WORKSPACE}/utils/platform_assesment/") {
                     sh '''
                     pip3 install -r requirements.txt
-                    python3 platform_assesment.py ${ELCM_URL} ${ANALYTICS_URL} ${REPORT_FILENAME}
+                    python3 platform_assesment.py ${ELCM_URL} ${ANALYTICS_URL} ${REPORT_FILENAME}.json
                     '''
                 }
+            }
+        }
+    }
+
+    post {
+        always {
+            retry(2) {
+                script {
+                    if ("${params.REPORTING}".toBoolean() == true) {
+                        sh '''#!/bin/bash
+                        if [ -f "${REPORT_FILENAME}.json" ]; then
+                            echo "$FILE exists."
+                        
+                            # get Commit Information
+                            cd $NETAPP_NAME_LOWER
+                            commit=$(git rev-parse HEAD)
+                            cd ..
+
+                            urlT=https://github.com/EVOLVED-5G/$NETAPP_NAME_LOWER/wiki/Telefonica-Evolved5g-$NETAPP_NAME_LOWER
+                            versionT=${VERSION}
+
+                            python3 utils/report_generator.py --template templates/${STAGE}/step-platform-assessment.md.j2 --json ${REPORT_FILENAME}.json --output $REPORT_FILENAME.md --repo ${GIT_NETAPP_URL} --branch ${GIT_NETAPP_BRANCH} --commit $commit --version $versionT --url $urlT --name $NETAPP_NAME --logs ${NETAPP_NAME_LOWER}-build-runtime_error.log
+                            docker run -v "$WORKSPACE":$DOCKER_PATH ${PDF_GENERATOR_IMAGE_NAME}:${PDF_GENERATOR_VERSION} markdown-pdf -f A4 -b 1cm -s $DOCKER_PATH/utils/docker_generate_pdf/style.css -o $DOCKER_PATH/$REPORT_FILENAME.pdf $DOCKER_PATH/$REPORT_FILENAME.md
+                            declare -a files=("json" "md" "pdf")
+
+                            for x in "${files[@]}"
+                                do
+                                    report_file="${REPORT_FILENAME}.$x"
+                                    url="$ARTIFACTORY_URL/$NETAPP_NAME_LOWER/$BUILD_ID/$report_file"
+
+                                    curl -v -f -i -X PUT -u $ARTIFACTORY_CRED \
+                                        --data-binary @"$report_file" \
+                                        "$url"
+                                done
+                        else
+                            echo "No report file generated"
+                        fi
+                        '''
+                    }
+                }
+            }
+            sh '''
+            docker ps -a -q | xargs --no-run-if-empty docker stop $(docker ps -a -q)
+            docker system prune -a -f --volumes
+            '''
+            script {
+                if ("${params.SEND_DEV_MAIL}".toBoolean() == true) {
+                    emailext body: '''${SCRIPT, template="groovy-html.template"}''',
+                mimeType: 'text/html',
+                subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
+                from: 'jenkins-evolved5G@tid.es',
+                replyTo: 'jenkins-evolved5G@tid.es',
+                recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+                }
+            }
+        }
+        cleanup {
+            /* clean up our workspace */
+            deleteDir()
+            /* clean up tmp directory */
+            dir("${env.workspace}@tmp") {
+                deleteDir()
+            }
+            /* clean up script directory */
+            dir("${env.workspace}@script") {
+                deleteDir()
             }
         }
     }
