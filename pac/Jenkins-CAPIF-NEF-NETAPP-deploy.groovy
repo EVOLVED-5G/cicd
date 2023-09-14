00211a53134c28301eaf4512a000a600136450dd
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2023-09-14 08:58:10 +0200

    Add Domain Name to TSN Helm deployment

--- pac/Jenkins-CAPIF-NEF-NETAPP-deploy.groovy
+++ pac/Jenkins-CAPIF-NEF-NETAPP-deploy.groovy
@@ -179,7 +179,8 @@ pipeline {
                             --arg NAMESPACE tsn-$BUILD_NUMBER --arg HOSTNAME_TSN $HOSTNAME_TSN \
                             --arg HOSTNAME_CAPIF $HOSTNAME_CAPIF --arg CAPIF_HTTP_PORT $CAPIF_HTTP_PORT \
                             --arg CAPIF_HTTPS_PORT $CAPIF_HTTPS_PORT --arg DEPLOYMENT $DEPLOYMENT \
-                            --arg CREATE_NS $CREATE_NS -f $WORKSPACE/cd/helm/helmfile.d/03-tsn.json \
+                            --arg CREATE_NS $CREATE_NS --arg DOMAIN_NAME $HOSTNAME_TSN:$CAPIF_HTTPS_PORT \
+                            -f $WORKSPACE/cd/helm/helmfile.d/03-tsn.json \
                             | yq -P > ./${BUILD_NUMBER}.d/03-tmp-tsn-${BUILD_NUMBER}.yaml
                             
                             echo "./${BUILD_NUMBER}.d/03-tmp-tsn-${BUILD_NUMBER}.yaml"
